a55d6cb9971e892b5982fda501b4aa7b093cecc4
nhcarrigan <nhcarrigan@gmail.com>
2020-07-23 09:56:39 -0700

    First page! :D

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      &copy;{" "}
+      <a
+        href="https://www.nhcarrigan.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Nicholas Carrigan
+      </a>
+    </footer>
+  );
+}
+
+export default Footer
