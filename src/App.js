f712fffd33e2105dbd39f90b9dd515bc62adf305
nhcarrigan <nhcarrigan@gmail.com>
2020-07-23 09:41:02 -0700

    Here we go!

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>My React Pages!</h1>
+      <p>
+        These are the pages I have created during my attempts to learn React
+        (including this page here! :D)
+      </p>
+      <ul>
+        <li>
+          <a href="">Dummy link for now...</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
